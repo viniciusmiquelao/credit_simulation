8cef2eed7371e8b6e4b7671b305a36e092b70d1f
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-06-23 16:44:00 -0300

    Add app widget in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'app/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CreditSimulationApp());
 }
 
 class MyApp extends StatelessWidget {
