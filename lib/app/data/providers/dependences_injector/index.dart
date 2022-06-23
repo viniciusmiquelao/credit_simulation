f9e21448fe988fc46bdb2bf128fd66d09e24e3ce
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-06-23 17:36:49 -0300

    Create DepencesInjector index

---
+++ lib/app/data/providers/dependences_injector/index.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+
+import 'modules/credit_simulation.dart';
+
+typedef FactoryFunc<T> = T Function();
+
+class DependencesInjector {
+  static final _locator = Get;
+
+  static T get<T extends Object>({String? instanceName}) {
+    return _locator.find(tag: instanceName);
+  }
+
+  static void registerFactory<T extends Object>(
+    FactoryFunc<T> factoryFunc, {
+    String? instanceName,
+  }) {
+    _locator.create(factoryFunc, tag: instanceName);
+  }
+
+  static void registerLazySingleton<T extends Object>(
+    FactoryFunc<T> factoryFunc, {
+    String? instanceName,
+  }) {
+    _locator.lazyPut(
+      factoryFunc,
+      tag: instanceName,
+    );
+  }
+
+  static void setup() {
+    setupCreditSimulationInjections();
+  }
+}
