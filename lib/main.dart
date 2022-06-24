8186e81e82eb8292dca8444a8f7dc23358b13941
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-06-23 22:44:55 -0300

    Add initial configs in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app/app.dart';
+import 'app/data/providers/dependences_injector/index.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  DependencesInjector.setup();
   runApp(CreditSimulationApp());
 }
