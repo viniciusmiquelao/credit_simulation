7105abc1dc0a01b7e9fa4ff6abb7762e87e498cc
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-06-23 22:43:52 -0300

    Integrate Personal Data Credit with controller

--- lib/app/ui/pages/credit_simulation/personal_data_credit_simulation_page.dart
+++ lib/app/ui/pages/credit_simulation/personal_data_credit_simulation_page.dart
@@ -1,7 +1,15 @@
 import 'package:credit_simulation/app/configs/constants/spacements.dart';
 import 'package:credit_simulation/app/configs/theme/custom_theme.dart';
+import 'package:credit_simulation/app/configs/validators/validate_full_name.dart';
+import 'package:credit_simulation/app/configs/validators/validator_email.dart';
+import 'package:credit_simulation/app/controllers/credit_simulation/credit_simulation_controller.dart';
+import 'package:credit_simulation/app/routes/routes.dart';
+import 'package:credit_simulation/app/ui/components/base_button.dart';
+import 'package:credit_simulation/app/ui/components/safe_scroll_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import '../../../data/providers/dependences_injector/index.dart';
 import '../../components/text_field_with_title.dart';
 
 class PersonalDataCreditSimulationPage extends StatefulWidget {
@@ -14,43 +22,85 @@ class PersonalDataCreditSimulationPage extends StatefulWidget {
 
 class _PersonalDataCreditSimulationPageState
     extends State<PersonalDataCreditSimulationPage> {
+  final _controller = DependencesInjector.get<CreditSimulationController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(Spacements.L),
+      body: SafeScrollView(
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Text.rich(
-                style: CustomTheme.of(context).textTheme!.h3,
-                TextSpan(
-                  children: [
-                    const TextSpan(text: 'Simule '),
-                    TextSpan(
-                      text: 'agora',
-                      style: TextStyle(
-                        color: CustomTheme.of(context).colors?.primary,
+              Container(
+                height: 300,
+                color:
+                    CustomTheme.of(context).colors!.primary!.withOpacity(.05),
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(Spacements.L),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text.rich(
+                            style: CustomTheme.of(context).textTheme!.h3,
+                            TextSpan(
+                              children: [
+                                const TextSpan(text: 'Simule '),
+                                TextSpan(
+                                  text: 'agora',
+                                  style: TextStyle(
+                                    color:
+                                        CustomTheme.of(context).colors?.primary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: Spacements.XS),
+                          const Text('Crédito, rápido, fácil e seguro! :)'),
+                        ],
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: Spacements.XL),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFieldWithTitle(
+                            title: 'Qual seu ',
+                            titleBold: 'nome completo?',
+                            onChanged: (e) => _controller.fullName.value = e,
+                            validator: validateFullName,
+                            hintText: 'Nome completo',
+                          ),
+                          const SizedBox(height: Spacements.M),
+                          TextFieldWithTitle(
+                            title: 'E seu ',
+                            titleBold: 'e-mail?',
+                            onChanged: (e) => _controller.email.value = e,
+                            validator: validateEmail,
+                            hintText: 'seuemail@email.com',
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: Spacements.XL),
+                      Obx(
+                        () => SafeArea(
+                          child: BaseButton(
+                            text: 'Continuar',
+                            onPressed: _controller.goToSteps,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: Spacements.S),
-              const Text('Crédito, rápido, fácil e seguro! :)'),
-              const SizedBox(height: Spacements.XL),
-              TextFieldWithTitle(
-                title: 'Qual seu ',
-                titleBold: 'nome completo?',
-                hintText: 'Nome completo',
-              ),
-              const SizedBox(height: Spacements.L),
-              TextFieldWithTitle(
-                title: 'E seu',
-                titleBold: 'e-mail?',
-                hintText: 'seuemail@email.com',
-              )
             ],
           ),
         ),
