43c697c30fef27980ec6c42136c38ec1c4ae23da
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-06-24 12:16:46 -0300

    Fix status bar in app

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,16 @@ import 'app/data/providers/dependences_injector/index.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness:
+          WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+                  Brightness.light
+              ? Brightness.dark
+              : Brightness.light,
+    ),
+  );
   DependencesInjector.setup();
   runApp(CreditSimulationApp());
 }
