c9a86e7d9019a932d04f2231ab7638fb49ecdb00
Vinícius <viniciuscarvalho60@yahoo.com.br>
2022-06-24 12:00:21 -0300

    Add Credit Simulation Repository in setup

--- lib/app/data/providers/dependences_injector/modules/credit_simulation.dart
+++ lib/app/data/providers/dependences_injector/modules/credit_simulation.dart
@@ -1,8 +1,19 @@
 import 'package:credit_simulation/app/controllers/credit_simulation/credit_simulation_controller.dart';
 import 'package:credit_simulation/app/data/providers/dependences_injector/index.dart';
+import 'package:credit_simulation/app/data/repositories/credit_simulation_repository.dart';
+
+import '../../http/http_client.dart';
 
 void setupCreditSimulationInjections() {
+  DependencesInjector.registerFactory<CreditSimulationRepository>(
+    () => CreditSimulationRepositoryImpl(
+      DependencesInjector.get<HttpClient>(),
+    ),
+  );
+
   DependencesInjector.registerFactory<CreditSimulationController>(
-    () => CreditSimulationController(),
+    () => CreditSimulationController(
+      DependencesInjector.get<CreditSimulationRepository>(),
+    ),
   );
 }
